6a84da09fd60dd12b446af21e952021560855769
Mikita <mikit.sazan@gmail.com>
2026-09-03 09:45:27 +0300

    Init Gradle application project (hexlet.code)

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+    id("io.github.ben-manes.versions") version "0.61.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
